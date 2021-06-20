0a38273ca070f0b3603c12ec1e5dd976bdc1ed5b
Jakub <jakubkrys@gmail.com>
2021-06-20 22:10:06 +0200

    #1 przygotowanie interfejsu funkcyjnego i wyrażeń lambda;'

---
+++ src/pl/jakubkrys/lambda_expressions/lambda_expressions_introduction/NumberTest.java
@@ -0,0 +1,19 @@
+package pl.jakubkrys.lambda_expressions.lambda_expressions_introduction;
+
+public class NumberTest {
+    public static void main(String[] args) {
+
+        System.out.println("----- isEven -----");
+        Validator isEven = (n) -> n%2 == 0;
+        System.out.println("23: "+isEven.validate(23));
+        System.out.println("46: "+isEven.validate(46));
+        System.out.println("9: "+isEven.validate(9));
+
+        System.out.println("----- isFromRange -----");
+        Validator isFromRange = (n) -> n >= 5 && n <= 55;
+        System.out.println("23: "+isFromRange.validate(23));
+        System.out.println("146: "+isFromRange.validate(146));
+        System.out.println("9: "+isFromRange.validate(9));
+        System.out.println("-122: "+isFromRange.validate(-122));
+    }
+}
